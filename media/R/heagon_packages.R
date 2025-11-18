644b31e6d597083c46a880fcf5ea6af596a71a8c
erikdeluca <erik4399@gmail.com>
2025-11-18 22:02:08 +0100

    R packages tracked by renv and about updated

--- media/R/heagon_packages.R
+++ media/R/heagon_packages.R
@@ -1,16 +1,19 @@
-library(hexsession)
+# WARNING
+# hexsession cannot be installed anymore with install.packages() and renv::install()
+
+# library(hexsession)
+# # make_tile()
+
+# library(tidyverse)
+# library(ggplot2)
+# library(tidymodels)
+# library(quarto)
+# # library(gt)45
+# library(gtsummary)
+# library(DT)
+# library(shiny)
+# library(quarto)
+
 # make_tile()
-
-library(tidyverse)
-library(ggplot2)
-library(tidymodels)
-library(quarto)
-# library(gt)45
-library(gtsummary)
-library(DT)
-library(shiny)
-library(quarto)
-
-make_tile()
-snap_tile(here::here("media", "packages.png"))
+# snap_tile(here::here("media", "packages.png"))
 
